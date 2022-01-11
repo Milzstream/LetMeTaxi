6194e326fc40252497cf4ea1b45e2181b909c09b
Elliott Quick <elliottquick@live.com>
2022-01-11 11:47:48 -0500

    Adding Method for CancelForm and updating code structure to support cancelform if form changes while talking to flightmaster

--- LetMeTaxi/LetMeTaxi.lua
+++ LetMeTaxi/LetMeTaxi.lua
@@ -1,15 +1,23 @@
+local function CancelForm()
+	DEFAULT_CHAT_FRAME.editBox:SetText("/cancelform")
+	ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
+end
+
+local function OnEvent(self, event, arg1, arg2, ...)
+	if event == "UI_ERROR_MESSAGE" then
+		if arg2 == ERR_TAXIPLAYERALREADYMOUNTED then
+			Dismount()
+		elseif arg2 == ERR_TAXIPLAYERSHAPESHIFTED then
+			CancelForm()
+		end
+	elseif event == "TAXIMAP_OPENED" then
+		Dismount()
+		CancelForm()
+	end
+end
+
 local f = CreateFrame("Frame", "LetMeTaxi", UIParent)
 f:RegisterEvent("TAXIMAP_OPENED")
-
-f:SetScript(
-	"OnEvent",
-	function(self, event, arg1, arg2, ...)
-		if event == "TAXIMAP_OPENED" then
-			Dismount()
-			DEFAULT_CHAT_FRAME.editBox:SetText("/cancelform")
-			ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
-		end
-	end
-)
-
+f:RegisterEvent("UI_ERROR_MESSAGE")
+f:SetScript("OnEvent", OnEvent)
 f:Hide()
